0433ab1d4e4e61c21f944db6b36bae416d1bfdab
Victor Dahlin <Victor@Victors-MacBook-Pro.local>
2016-04-14 17:24:52 +0200

    404 page and error every day

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.master')
+@section('content')
+
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12">
+                <h1 class="text-center">404</h1>
+                <h1 class="text-center">Sidan du söker är inte tillgänglig</h1>
+            </div>
+        </div>
+    </div>
+@stop
